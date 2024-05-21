4fe49edafc07894b391b5090084271c056003e05
Gazeux33-IUT <theocastillo@yahoo.com>
2024-05-21 09:07:47 +0200

    clean utils

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,3 @@
-from datetime import datetime
-
-import torch
-from PIL import Image
 import numpy as np
 import os
 import cv2 as cv
@@ -15,9 +11,7 @@ IMAGENET_STD_NEUTRAL = [1, 1, 1]
 
 def load_image(img_path, target_shape=None):
     if not os.path.exists(img_path):
-        raise Exception(f'Path does not exist: {img_path}')
-    img = Image.open(img_path).convert('RGB')
-
+        raise FileNotFoundError(f'Path does not exist: {img_path}')
     img = cv.imread(img_path)[:, :, ::-1]
 
     if target_shape is not None:
@@ -43,32 +37,5 @@ def load_image(img_path, target_shape=None):
 
 
 
-
-
-
-def get_name(content_name, style_name):
-    now = datetime.now()
-    formatted_datetime = now.strftime('%Y-%m-%d-%H:%M:%S')
-    name = content_name[:content_name.index(".")] + "_" + style_name[:style_name.index(".")] + formatted_datetime
-    return name
-
-
-def display_image(img):
-    plt.imshow(img)
-    plt.axis("off")
-    plt.show()
-
-
-
-
-
-def get_uint8_range(x):
-    if isinstance(x, np.ndarray):
-        x -= np.min(x)
-        x /= np.max(x)
-        x *= 255
-        return x
-    else:
-        raise ValueError(f'Expected numpy array got {type(x)}')
 
 
